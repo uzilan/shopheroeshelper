f7f1f6029639c49c729f7aba093c48c0da837a89
Uzi Landsmann <uzi.landsmann@gmail.com>
2016-04-15 01:19:23 +0200

    refactored and documented the isSpecSelected method in the worker controller

--- app/scripts/controllers/worker.js
+++ app/scripts/controllers/worker.js
@@ -20,17 +20,25 @@ angular.module('shopheroesApp')
         .join(', ')
         .value();
     };
+    // find out if a given specialization is among the ones that the selected workers have
     $scope.isSpecSelected = function (specialization) {
 
-      var zip = _.zip(workerStructure.workers, $scope.workers);
-      var filtered = _.filter(zip, function (pair) {
-        return pair[1];
-      });
-      var specList = _.flatMap(filtered, function (f) {
-        return f[0].specializations;
-      });
-      var found = _.find(specList, {'name': specialization.name});
+      // take the list of all workers: {name: 'Armorer',tier: 1,specializations: [spec.armor, spec.metal]}...
+      return _.chain(workerStructure.workers)
+        // zip it with the list of selected workers: [[{name: 'Armorer'...}, false], [{name: 'Blacksmith'...}, true]...]
+        .zip($scope.workers)
+        // filter it so that only the selected workers are left: [[{name: 'Blacksmith'...}, true]...]
+        .filter(function (pair) {
+          return pair[1]; // that is, if the second element in the array (which indicates selection) exists and is true
+        })
+        // flatmap it to get a single list of chosen specializations: [spec.armor, spec.metal]
+        .flatMap(function (f) {
+          return f[0].specializations;
+        })
+        // try to find the given specialization among the list above
+        .find({'name': specialization.name})
+        // voila! (oh, and when chaining, lodash wants us to call the value() function at the end)
+        .value();
 
-      return found;
     }
   }]);
